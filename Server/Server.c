b1f33ae7514a5e68b729249b8e3f21efe12898d3
Mitchell Jacobs <mrjacobs@wpi.edu>
2019-09-09 12:01:45 -0400

    Server Interperts Header

--- Server/Server.c
+++ Server/Server.c
@@ -17,92 +17,147 @@
 
 #include <errno.h>
 
-#define PORT 3534
+
 
 int main(int argc, char *argv[]){
+	int port;
+
+	if(argv[1] == NULL){
+		port = 8080;
+		printf("Invalid Port Number Using Default Value of %i\n", port);
+	}else{
+		port = atoi(argv[1]);
+		if(port > 65535 || port <= 0){
+			port = 8080;
+			printf("Invalid Port Number Using Default Value of %i\n", port);
+		}
+	}
+
 	int sock = socket(AF_INET, SOCK_STREAM,0);
+
 	if(sock < 0){
-		printf("Socket Creation Failed\n");
+		fprintf(stderr, "Error creating socket: %s\n", strerror(errno));
 		exit(0);
 	}else{
-		printf("Socket opened...\n");
+		//printf("Socket opened...\n");
 
 	}
 
 	struct sockaddr_in client_addr;
 	client_addr.sin_family = AF_INET;
 	client_addr.sin_addr.s_addr = INADDR_ANY;
-	client_addr.sin_port = htons(PORT);
+	client_addr.sin_port = htons(port);
 	int addrlen = sizeof(client_addr);
 
 	int opt = 1;
 	int freeSockStatus = setsockopt(sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
 
 	if(freeSockStatus < 0){
-		printf("Error Force Freeing Socket");
+		fprintf(stderr, "Error freeing socket: %s\n", strerror(errno));
 		exit(0);
 	}
 
 	int bindStatus = bind(sock , (const struct sockaddr *)&client_addr, addrlen);
 
 	if(bindStatus < 0){
-		printf("Binding Failed\n");
+		fprintf(stderr, "Error binding socket: %s\n", strerror(errno));
 		exit(0);
 	}else{
-		printf("Binded to port ...\n");
+		//printf("Binded to port ...\n");
 	}
 
 	int listenStatus = listen(sock,5);
 	if(listenStatus<0){
-		printf("Listen Error\n");
+		fprintf(stderr, "Error listening on socket: %s\n", strerror(errno));
 		exit(0);
 	}else{
-		printf("Listening...\n");
+		//printf("Listening...\n");
 	}
-
+	printf("Server is running on port %i\n", port);
 
 	int connection = accept(sock, (struct sockaddr *)&client_addr, (socklen_t*)&addrlen);
 	if(connection < 0){
-		printf("Connection Error\n");
+		fprintf(stderr, "Error listening connecting to client: %s\n", strerror(errno));
 		exit(0);
 	}else{
-		printf("Connecting...\n");
+		//printf("Connecting...\n");
 	}
 
 	char buffer[1024] = {0};
 	int readStatus = read(connection, buffer, 1024);
-	printf("%s\n",buffer);
 
-	//send(connection, buffer, strlen(buffer), 0);
-	transmitFile(connection);
+	if(readStatus < 0){
+		fprintf(stderr, "Error reading from buffer: %s\n", strerror(errno));
+	}
+	//printf("%s\n",buffer);
 
-	printf("Readback sent\n");
+	char function[8] = {0};
+	char file[32] = {0};
+
+	proccess(buffer, function, file);
+
+
+	//printf("%s\n%s\n\n", function, file);
+
+
+	//send(connection, buffer, strlen(buffer), 0);  //ECHO SERVER
+	if(strcmp(function,"GET")){
+		get(file, connection);
+	}
+
+	//printf("Readback sent\n");
 
 	int closeSocketStatus = shutdown(connection,2) | shutdown(sock,2) ;
 
 	return closeSocketStatus;
 }
 
-int transmitFile(int connection){
+int get(char* name, int connection){
+	char* filename = name;
+	FILE *file = fopen (filename, "r");
+	char* header;
 
-	char* header = "HTTP/1.1 200 OK \r\n \r\n";
-	send(connection, header, strlen(header), 0);
-	static const char filename[] = "TMDG.html";
+	if(file != NULL){
+		header = "HTTP/1.1 200 OK \r\n \r\n";
+	}else{
+		header = "HTTP/1.1 404 Not Found \r\n \r\n";
+	}
+	send(connection, header, strlen(header), 0); //Send header
 
-	   FILE *file = fopen ( filename, "r" );
-	   if ( file != NULL )
-	   {
-	      char line [16384]; /* or other suitable maximum line size */
-	      while ( fgets ( line, sizeof line, file ) != NULL ) /* read a line */
-	      {
-	         send(connection, line, strlen(line), 0);
-	      }
-	      fclose ( file );
-	   }
-	   else
-	   {
-	      perror ( filename ); /* why didn't the file open? */
-	   }
-	   return 0;
+	if (file != NULL)
+	{
+		char line [1024];
+		while (fgets (line, sizeof line, file) != NULL ) // Read file chunk
+		{
+			send(connection, line, strlen(line), 0); //Transmit file chunk
+		}
+		fclose (file);
+	}
+	else
+	{
+		fprintf(stderr, "404 sent: %s\n", strerror(errno));
+	}
+	return 0;
 }
 
+void proccess(char* request, char* function, char* file){
+	char* next = request;
+	char* tracker = function;
+	while(*next != ' ' && tracker < (function+8)){
+		*tracker = *next;
+		next++;
+		tracker ++;
+	}
+	tracker = '\0';
+	next ++;
+	next ++;
+	tracker = file;
+	while(*next != ' ' && tracker < (file+32)){
+		*tracker = *next;
+		next++;
+		tracker ++;
+	}
+	tracker = '\0';
+
+
+}
--- Server/Server.h
+++ Server/Server.h
@@ -3,6 +3,7 @@
 #define SERVER_H_
 
 int main(int argc, char *argv[]);
-int transmitFile(int connection);
+int get(char* name, int connection);
+void proccess(char* request, char* function, char* file);
 
 #endif
